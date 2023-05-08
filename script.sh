2ddfd1d049bd7c79a2d0c8340dd3db1883452d19
Kevin <kevinbalvarez@gmail.com>
2023-05-08 07:28:45 -0400

    More nginx configuration

--- script.sh
+++ script.sh
@@ -2,11 +2,13 @@
 FILE=/app/django_app/
 if [ -d "$FILE" ]; then
     cd /app/django_app
+    python manage.py collectstatic --no-input 
     gunicorn django_app.wsgi:application --bind 0.0.0.0:8000
 else
     django-admin startproject django_app \
     && cd /app/django_app \
     && python manage.py migrate \
+    && python manage.py collectstatic --no-input \
     && python manage.py createsuperuser --no-input \
     && gunicorn django_app.wsgi:application --bind 0.0.0.0:8000
 fi
